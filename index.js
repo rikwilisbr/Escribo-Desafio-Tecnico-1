134a4717ba271615785efe11afed5988ccd9383c
Henrique William <104221714+rikwilisbr@users.noreply.github.com>
2023-11-29 08:46:18 -0300

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function SomaDivisiveis(numero){
+function SomaDivisiveis(num){
     let soma = 0
 
-    for (let i = 0; i < numero; i++ ){
+    for (let i = 0; i < num; i++ ){
         if(i % 3 === 0 || i % 5 === 0){
             soma += i
         }
@@ -10,6 +10,12 @@ function SomaDivisiveis(numero){
     return soma
 }
 
-const numero = 10;
-const resultado = SomaDivisiveis(numero);
-console.log(`O somatório dos números divisíveis por 3 ou 5 até ${numero} é: ${resultado}`)
+const numero = 11;
+const parseIntNumero = parseInt(numero)
+
+if(parseIntNumero < 0){
+    console.log('O número deve ser positivo')
+} else {
+    const resultado = SomaDivisiveis(numero);
+    console.log(`O somatório dos números divisíveis por 3 ou 5 até ${parseIntNumero} é: ${resultado}`)
+}
